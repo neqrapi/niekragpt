81ea049f92a679438260611ec9ebf299ebf1ee56
neqrapi <piotrek.niekra@gmail.com>
2023-11-17 20:24:23 +0100

    Folder setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    
+    <Button>Click Me</Button>
+  )
+}
+export default DashboardPage
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const LadningPage = () => {
+  return (
+    
+    <div>
+        LP(Unprotected)
+    </div>
+  )
+}
+export default LadningPage 
